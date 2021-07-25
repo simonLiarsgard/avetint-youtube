013619236625dc6d8a3bb7694f2f90b1cf4dd106
liars99 <36644986+liars99@users.noreply.github.com>
2021-07-25 16:58:35 +0200

    Nice

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	const YOUTUBE_URL = 'https://www.youtube.com/channel/UCUqcGXhbJGakSs0YG-xF5fg';
+
+	export const load: Load = () => {
+		return {
+			status: 302,
+			redirect: YOUTUBE_URL
+		};
+	};
+</script>
